15645ccd923b67b364df4cabe755205905d539e1
Andrew Van Til <avantil@gmail.com>
2019-05-27 17:05:30 -0500

    Update openalpr-install.ps1

--- openalpr-install.ps1
+++ openalpr-install.ps1
@@ -2,7 +2,6 @@
 cd openalpr
 git clone https://github.com/peters/openalpr-windows.git windows
 cd windows
-git checkout v2.1.0
 git submodule update --init --recursive
 
 .\build.ps1 -Configuration Release -Platform x64 -PlatformToolset v120 -CudaGeneration None
